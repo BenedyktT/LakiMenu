53e46411439fea35b10017f270926f6c432020a5
BenedyktT <benedykt.tyminski@gmail.com>
2019-05-11 22:03:06 +0000

    body except footer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
 import style from "./styles/main.css";
 import './styles/appStyles.scss';
+import Glide from '@glidejs/glide'
 
+let carousel = new Glide('.glide',{
+    type:"carousel",
+    hoverpause: true,
+    gap: 10,
+    startAt:0,
+    perView:3,
+    autoplay:4000,
+    animationDuration: 1500,
+    peek: {
+        before: 50,
+        after: 100
+      },
+    breakpoints: {
+    1630: {
+        peek:{
+            before:0,
+            after:0
+        }
+    },
+    1024: {
+        perView: 2
+        
+    },
+    600: {
+        perView: 1,
+        peek: {
+            before:0,
+            after:0
+        }
+    },
+    
+    },
+    
+})
+/* glide.on('mount.after', function () {
+    // Logic fired after mounting
+  }) */
+carousel.mount()
 
+const slide = document.querySelectorAll('glide__slide img')
+slide.forEach((e)=>e.style.opacity=0)
 
 let hamburger = document.querySelector('.hamburger-container')
 hamburger.addEventListener('click', ()=>{
