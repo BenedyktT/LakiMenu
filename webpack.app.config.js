9d8a2bcb0b51f461fe93aaf3b977b9974094f9fd
Benedykt Tyminski <benedykt.tyminski@gmail.com>
2019-08-02 19:03:27 +0000

    test without image optimisation

--- webpack.app.config.js
+++ webpack.app.config.js
@@ -101,11 +101,11 @@ target: 'web',
         chunkFilename: "[id].css"
       }),
   
-                      new ImageminPlugin({
+/*                       new ImageminPlugin({
         pngquant: {quality: '50-60'},
         plugins: [
-          imageminMozjpeg({progressive:false,quality: 50}),
-          imageminWebp({autoFilter:true,resize:{width:600,height:0}})]
-      })        
+          imageminMozjpeg({progressive:true,quality: 50}),
+          imageminWebp({autoFilter:true,resize:{width:800,height:0}})]
+      })  */       
   ]
 };
