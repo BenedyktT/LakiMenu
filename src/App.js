cd968d524fbe0232e23c7e60eb2f1ea84b3dac36
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2019-12-21 23:09:21 +0000

    implementing christmas modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import "./styles/App.scss";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
@@ -9,23 +9,61 @@ import { Provider } from "./context";
 import Success from "./components/Success";
 import Menu from "./components/Menu";
 import Location from "./components/Location";
+import Modal from "react-modal";
+import BuffetAd from "./images/buffet-ad.png";
+const customStyles = {
+	content: {
+		top: "50%",
+		left: "50%",
+		right: "auto",
+		bottom: "auto",
+		marginRight: "-50%",
+		transform: "translate(-50%, -50%)",
+		zIndex: "1000"
+	}
+};
+Modal.setAppElement("#app");
 
 const App = () => {
-  return (
-    <Provider>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/reserve" component={Reserve} />
-          <Route exact path="/success" component={Success} />
-          <Route exact path="/menu" component={Menu} />
-          <Route exact path="/location" component={Location} />
-        </Switch>
-        <Footer />
-      </Router>
-    </Provider>
-  );
+	let subtitle;
+	const [modalIsOpen, setModalOpen] = useState(true);
+	const openModal = () => {
+		setModalOpen(true);
+	};
+	const closeModal = () => {
+		setModalOpen(false);
+	};
+
+	return (
+		<Provider>
+			<Router>
+				<Navbar />
+				<Modal
+					isOpen={modalIsOpen}
+					onRequestClose={closeModal}
+					style={customStyles}
+					contentLabel="Example Modal"
+				>
+					<button className="btn-modal" onClick={closeModal}>
+						x
+					</button>
+					<div className="modal-content">
+						<div className="modal-bg">
+							<img src={BuffetAd} alt="" />
+						</div>
+					</div>
+				</Modal>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/reserve" component={Reserve} />
+					<Route exact path="/success" component={Success} />
+					<Route exact path="/menu" component={Menu} />
+					<Route exact path="/location" component={Location} />
+				</Switch>
+				<Footer />
+			</Router>
+		</Provider>
+	);
 };
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,25 +8,25 @@ import Gallery from "./mainPage/Gallery";
 import { HashLink } from "react-router-hash-link";
 
 const Home = () => {
-  return (
-    <Fragment>
-      <header className="header">
-        <div className="header-bg"></div>
-        <div id="to-about" className="arrow">
-          <HashLink to="/#to-about">
-            <Arrow />
-          </HashLink>
-        </div>
-      </header>
-      <main>
-        <About />
-        <Menu />
-        <Location />
-        <Testimonials />
-        <Gallery />
-      </main>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<header className="header">
+				<div className="header-bg"></div>
+				<div id="to-about" className="arrow">
+					<HashLink to="/#to-about">
+						<Arrow />
+					</HashLink>
+				</div>
+			</header>
+			<main>
+				<About />
+				<Menu />
+				<Location />
+				<Testimonials />
+				<Gallery />
+			</main>
+		</Fragment>
+	);
 };
 
 export default Home;
