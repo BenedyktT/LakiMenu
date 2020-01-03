5fbe41f5d4fc4faf232b1d00ea7c9261f50be144
BenedyktT <benedykt.tyminski@gmail.com>
2020-01-03 22:14:00 +0000

    scroll to top on router

--- src/App.js
+++ src/App.js
@@ -1,74 +1,34 @@
-import React, { useState, useContext, useReducer } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import "./styles/App.scss";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Reserve from "./components/Reserve";
-import { Provider, ModalContext, modalReducer, initialState } from "./context";
+import { Provider } from "./context";
 import Success from "./components/Success";
 import Menu from "./components/Menu";
 import Location from "./components/Location";
-import Modal from "react-modal";
-import BuffetAd from "./images/buffet-ad.png";
+import ScrollToTop from "react-router-scroll-top";
 import Cocktails from "./components/Cocktails";
-Modal.defaultStyles.overlay.backgroundColor = "#8176768e";
-Modal.defaultStyles.overlay.zIndex = "5";
-const customStyles = {
-  content: {
-    zIndex: "8",
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    padding: 0,
-    backgroundColor: "#8176768e",
-    border: "none"
-  }
-};
-Modal.setAppElement("#app");
 
 const App = () => {
-  const useModalState = useReducer(modalReducer, initialState);
-  const [state, dispatch] = useModalState;
-
-  const { modalIsOpen } = state;
-  const openModal = () => {
-    dispatch({ type: "SET_MODAL_OPEN" });
-  };
-  const closeModal = () => {
-    dispatch({ type: "SET_MODAL_CLOSE" });
-  };
-
   return (
-    <Provider value={useModalState}>
+    <Provider>
       <Router>
-        <Navbar />
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
-          <button className="btn-modal" onClick={closeModal}>
-            x
-          </button>
+        <ScrollToTop>
+          <Navbar />
 
-          <div className="modal-bg">
-            <img className="buffet-img" src={BuffetAd} alt="" />
-          </div>
-        </Modal>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/reserve" component={Reserve} />
-          <Route exact path="/success" component={Success} />
-          <Route exact path="/menu" component={Menu} />
-          <Route exact path="/location" component={Location} />
-          <Route exact path="/bar" component={Cocktails} />
-        </Switch>
-        <Footer />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/reserve" component={Reserve} />
+            <Route exact path="/success" component={Success} />
+            <Route exact path="/menu" component={Menu} />
+            <Route exact path="/location" component={Location} />
+            <Route exact path="/bar" component={Cocktails} />
+          </Switch>
+          <Footer />
+        </ScrollToTop>
       </Router>
     </Provider>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,20 +10,17 @@ import { ModalContext } from "../context";
 import Cocktails from "./mainPage/Cocktails";
 
 const Home = () => {
-  const [state] = useContext(ModalContext);
-  const { modalIsOpen } = state;
-
   return (
     <Fragment>
       <header className="header">
         <div className="header-bg"></div>
-        {!modalIsOpen && (
+        {
           <div id="to-about" className="arrow">
             <HashLink to="/#to-about">
               <Arrow />
             </HashLink>
           </div>
-        )}
+        }
       </header>
       <main>
         <About />
